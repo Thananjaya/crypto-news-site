9d9c1c8397b8a69ab9ff15a7f5a4d46f4a8eb51c
Thananjaya <thananjaya091993@gmail.com>
2019-03-15 16:02:55 +0530

    adding news view

--- cryptonews/views.py
+++ cryptonews/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+import requests
+import json
 
 def home(request):
-	return render(request, 'cryptonews/home.html', {})
+	news_request = requests.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
+	data = json.loads(news_request.content)
+	return render(request, 'cryptonews/home.html', {'request': data})
